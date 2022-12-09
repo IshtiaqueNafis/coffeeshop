62f83a26c090473d5ec2ac82ac6ecbe39daa7dc6
FJalaliAmeri <f.jalali.ameri@gmail.com>
2022-12-09 16:20:32 -0500

    About.jsx updated

--- Screens/About.jsx
+++ Screens/About.jsx
@@ -1,24 +1,63 @@
-import React, {useState} from 'react';
-import {data} from "../api/aboutUserdata";
-import {Box, Center} from "native-base";
+import React, { useState } from "react";
+import { data } from "../api/aboutUserdata";
+import {
+  Box,
+  Text,
+  Heading,
+  VStack,
+  FormControl,
+  Input,
+  Link,
+  Button,
+  HStack,
+  Center,
+  NativeBaseProvider,
+  extendTheme,
+  TextArea,
+  ScrollView,
+  Icon,
+  ArrowUpIcon,
+} from "native-base";
 import Basic from "./Basic";
 
 const About = () => {
-
-    const [userData, setUserData] = useState(data)
-    const onRowDidOpen = rowKey => {
-        console.log('This row opened', rowKey);
-    };
-    return (
-
-        <Box textAlign="center" bg="white" flex={4} safeAreaTop>
-
-                <Basic/>
-
-        </Box>
-
-
-    );
+  const [userData, setUserData] = useState(data);
+  const onRowDidOpen = (rowKey) => {
+    console.log("This row opened", rowKey);
+  };
+  return (
+    <ScrollView>
+      <Box safeArea mx="5" w="90%" alignItems="center">
+        <Heading fontSize="5xl" color="primary.50">
+          About Us
+        </Heading>
+        <Text fontSize="xl" color="primary.100" mt="10">
+          Coffee Shops Guide was launched in November 2022 in Toronto, Canada.
+          We are a Mobile App Developer Team. We want you to enjoy your coffee
+          without worrying about the place, Coffee Shop Guide simplifies your
+          search for coffee shops quickly and efficiently.
+        </Text>
+        <Text fontSize="xl" color="primary.100" mt="10">
+          Developers:
+        </Text>
+        <Text fontSize="xl" color="primary.100">
+          Israr Wahid - 101348701
+        </Text>
+        <Text fontSize="xl" color="primary.100">
+          Sarah Sami - 101334588
+        </Text>
+        <Text fontSize="xl" color="primary.100">
+          Farshad Jalali Ameri - 101303158
+        </Text>
+        <Text fontSize="xl" color="primary.100">
+          Nafis Ishtiaque - 101206872
+        </Text>
+        <Text fontSize="xl" color="primary.100">
+          Roberto Borges - 101255891
+        </Text>
+      </Box>
+    </ScrollView>
+  );
 };
 
 export default About;
